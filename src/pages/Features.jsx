924e3a0266ecd6548f8c553ff9bf6a78e7a8ee54
Sahilpreet33 <117150951+Sahilpreet33@users.noreply.github.com>
2023-07-20 12:22:06 +0530

    changes

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -4,7 +4,8 @@ const Features = () => {
   return (
     <>
         <div>
-            <h1 className='text-slate-900 text-5xl'>This is feature page.</h1>
+            <h1 className='text-slate-900 text-5xl'>This is feature page. hlooo</h1>
+            <h1>hlooo</h1>
         </div>
     </>
   )
