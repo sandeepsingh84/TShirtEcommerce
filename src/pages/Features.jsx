1e4cd80986d573ceec23d69de7e90749404194f8
karan987thind <thindkaran987@gmail.com>
2023-07-20 10:59:40 +0530

    new update

---
+++ src/pages/Features.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Features = () => {
+  return (
+    <>
+        <div>
+            <h1 className='text-slate-900 text-5xl'>This is feature page.</h1>
+        </div>
+    </>
+  )
+}
+
+export default Features
