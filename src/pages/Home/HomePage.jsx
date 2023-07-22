9b39a0b97d565da61f8c4f8c5d8a3f00f064d1de
karan987thind <thindkaran987@gmail.com>
2023-07-22 11:49:14 +0530

    update

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import React, { useState } from "react";
-import { ArrowIcon } from "@/utils/appIcons";
+// import '@/Home/Home.css'
 
 const Homepages = () => {
   const [picture, setPicture] = useState("t-shirt4.png");
-  const [image, setImage] = useState("");
+const [image, setImage] = useState("")
   console.log("picture", picture);
   function MyTshirt(shirt) {
     setPicture(shirt);
@@ -98,10 +98,10 @@ const Homepages = () => {
           Styles Clothes
         </div>
 
-        <div className="flex pt-24 items-center  justify-center  w-[100%] m-auto">
+        <div className="flex pt-24   justify-center  w-[100%] m-auto bg-sky-100">
           <div className="w-[50%] h-[500px] bg-red-00 m-auto">
             <iframe
-              className="w-[100%] h-[100%] shadow-2xl"
+              className="w-[100%] h-[100%]"
               src="https://player.vimeo.com/video/512170000?h=64d3622777&autoplay=1&loop=1&byline=0&portrait=0"
               frameborder="0"
               allow="autoplay; fullscreen;"
@@ -111,13 +111,11 @@ const Homepages = () => {
 
           <div className="w-[35%]">
             <div>
-              <p className="text-4xl  mb-5 font-semibold font-[san-sarif] ">
-                Clothes
-              </p>
+              <p className="text-2xl font-semibold ">Clothes</p>
             </div>
 
             <div className="w-[70%]">
-              <p className="text-xl text-gray-500">
+              <p className="text-xl">
                 Clothing are items used to cover the human body. Humans are the
                 only animals that wear clothing. During the many thousands of
                 years between losing body hair and learning to make clothes,
@@ -129,79 +127,47 @@ const Homepages = () => {
         </div>
       </div>
 
-      {/* the right choice   */}
+      {/* onclick change photo and text  */}
 
-      <div className=" bg-gradient-to-r from-blue-100 to-fuchsia-100    w-full h-[850px] ">
-        <div className="flex justify-center gap-10 h-[100vh] w-full mt-28 px-48 pt-32">
-          <div className="w-[50%] ">
-            <p className="font-semibold text-xl mb-5">THE RIGHT CHOICE </p>
-            <div className="text-4xl font-semibold mb-10">
-              <h1>We are the Best T-shirt Printing</h1>
-              <h1>Company in Town, Trust Us!</h1>
-            </div>
-            <p className="text-slate-600">
-              There is no easy way to be sure when clothing was first developed,
-              because it was prehistoric and clothing is perishable. One of the
-              earliest that has been found is a cloak made of the fur of
-              squirrels, from a cave in Italy, dated to 23.000 years ago.[2]
-              Some estimates come from studying the biology of lice. The body
-              louse lives in clothing, and diverged from head lice about 107,000
-              years ago.{" "}
-            </p>
+      <div>
+        <div>
+          <p>tshirt</p>
+          <p> jacket</p>
+          <p>shrit</p>
+        </div>
 
-            <div className=" justify-start mt-10  ">
-              <div className="flex justify-center gap-14 text-4xl font font-semibold ">
-                <h1 className="justify-start flex px-10 -ml-20">5.200+</h1>
-                <h1>14K+</h1>
-              </div>
+        <div className="flex ">
+          <Image
+            src={{ image }}
+            width={500}
+            height={500}
+            className="w-[400px]"
+            id="big-img"
+          />
 
-              <div className="flex justify-center gap-12 mt-5 mb-5">
-                <p>Sold Goods by Product</p>
+          <Image
+            width={500}
+            height={500}
+            src="/Images/t-shirt4.png"
+            alt=""
+            className="w-52"
+          />
 
-                <p>Trusted by Client</p>
-              </div>
-            </div>
+          <Image
+            width={500}
+            height={500}
+            src="/Images/tshirt1.png"
+            alt=""
+            className="w-52 mt-5"
+          />
 
-            <div className="flex justify-center items-center gap-2 bg-sky-400 w-fit m-auto p-2 rounded-lg text-white hover:bg-white hover:text-sky-400 border ">
-              <p>Read More </p>
-              <ArrowIcon />
-            </div>
-          </div>
-
-          <div className="w-[50%] flex gap-5 justify-center item-center">
-            <div className="w-[250px] bg-gray-200 p-4 h-72 mt-36 rounded-lg hover:bg-orange-200 ">
-              <img src="Images/Carttt.png" className="w-20 m-auto" />
-
-              <h1 className="text-xl  font-semibold">Print T-shirt Youself</h1>
-              <p>
-                personalitie in. Clothing is far more than just a means to
-                protect our bodies.{" "}
-              </p>
-            </div>
-
-            <div className="w-[250px] bg-gray-200 p-4 h-72 rounded-lg hover:bg-pink-200 hover:h-72 ">
-              <img src="Images\iconcomputer.png" className="w-20 m-auto" />
-
-              <h1 className="text-xl  font-semibold">Your Online Business</h1>
-
-              <p>
-                Clothing is usually made of fabric sewn together, but may also
-                be animal skins. Each body part has a typical item of clothing.{" "}
-              </p>
-
-              <div className="w-[250px] bg-gray-200 p-4 mt-16 -ml-4 rounded-lg hover:bg-sky-200 hover:h-72">
-                <img src="Images\iconphone.png" className="w-20 m-auto " />
-
-                <h1 className="text-xl  font-semibold">Your Online Business</h1>
-
-                <p>
-                  Clothing is usually made of fabric sewn together, but may also
-                  be animal skins. Each body part has a typical item of
-                  clothing.{" "}
-                </p>
-              </div>
-            </div>
-          </div>
+          <Image
+            width={500}
+            height={500}
+            src="/Images/t-shirt2.png"
+            alt="/Images/jacket2.png"
+            className="w-52"
+          />
         </div>
       </div>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,19 @@
 // import '@/Home/Home.css'
-import { Inter } from 'next/font/google'
-import Homepages from '@/Home/Homepages'
-import Service from './service/Service'
-import About from './About'
-import Navbar from './Navbar/Navbar'
-import ShirtCart from '@/Components/ShirtCart'
-import Swipercode from '@/Components/Swipercode'
+import { Inter } from "next/font/google";
+import Service from "./service/Service";
+import About from "./About";
+import Navbar from "./Navbar/Navbar";
+import ShirtCart from "@/Components/ShirtCart";
 
-
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
-      <Navbar/>
-      <Homepages />
-      <Service/>
-        <About/>
-        <ShirtCart/>
-        <Swipercode/>
-  
+      <Navbar />
+      <Service />
+      <About />
+      <ShirtCart />
     </main>
   );
 }
