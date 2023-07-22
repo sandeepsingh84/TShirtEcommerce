8c536f196d7814474548565cf3112e20a97df289
karan987thind <thindkaran987@gmail.com>
2023-07-22 13:29:28 +0530

    update

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,8 +1,128 @@
 import React from 'react'
+// import img from "../../public/Images/star.png"
 
 const SingleProduct = () => {
   return (
-    <div>SingleProduct</div>
+    <div>
+
+    <div>
+
+<div>
+  <img src='https://www.beyoung.in/api/cache/catalog/products/polo_new_update_images_10_1_2022/lush_green_men_base_19_05_2023_700x933.jpg'/>
+</div>
+
+
+
+
+<div>
+<div>
+  <p>Lush Green Polo T-shirt</p>
+  <img src='heart.png' className='w-6'/>
+  </div>
+
+
+  <h1>$549</h1>
+  <p>Inclusive of All Taxes + Free Shipping</p>
+
+<div>
+  <img src="/star1.png"/>
+  <p>5.0 (8 Reviews)</p>
+  </div>
+
+
+
+<div>
+  <img src='discount.png' className='w-14'/>
+  Extra $ 100 OFF on $ 999 (Code:BEYOUNG 100)
+</div>
+
+
+<div>
+
+<h1>COLOR</h1>
+
+<div className='flex gap-1'>
+  <div className='border rounded-full h-8 w-8 bg-red-600 border-black'></div>
+
+
+<div className='border rounded-full h-8 w-8 bg-pink-600 border-black'></div>
+
+
+<div className='border rounded-full h-8 w-8 bg-sky-600 border-black'></div>
+
+
+
+<div className='border rounded-full h-8 w-8 bg-orange-600 border-black'></div>
+
+
+
+<div className='border rounded-full h-8 w-8 bg-purple-600 border-black'></div>
+
+
+<div className='border rounded-full h-8 w-8 bg-yellow-600 border-black'></div>
+
+
+<div className='border rounded-full h-8 w-8 bg-white border-black'></div>
+
+
+
+
+</div>
+
+
+  </div>
+
+
+
+
+
+<div>
+  <h1>SIZE</h1>
+  <p>SIZE CHART</p>
+</div>
+
+
+
+
+<div className='flex gap-1'>
+
+  <button className='border w-8 h-8 border-black rounded-full text-center'>
+    M
+  </button>
+
+
+  <button className='border w-8 h-8 border-black rounded-full text-center'>
+L
+  </button>
+
+
+  <button className='border w-8 h-8 border-black rounded-full text-center '>
+    XL
+  </button>
+
+
+
+  <button className='border w-8 h-8 border-black rounded-full text-center text-xs'>
+    XXL
+  </button>
+
+
+  <button className='border w-9  h-9 border-black rounded-full text-center text-xs'>
+    XXXL
+  </button>
+
+</div>
+
+
+
+
+
+
+    </div>
+    
+    
+    </div>
+    </div>
   )
 }
 
